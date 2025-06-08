cadaf70f3f51c0a90c46950bcb40a0360a184907
rohitchauhan533 <chauhanrohit533@gmail.com>
2025-06-08 23:43:12 +0530

    clear cart logic implemented

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,14 +1,24 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import ItemList from './ItemList'
+import { Button } from '@mui/material'
+import { clearCart } from '../utils/cartSlice'
 
 const Cart = () => {
+    const dispatch = useDispatch()
 
     const cartItems = useSelector((store) => store.cart.items)
+
+    const handleClearCart = () => {
+        dispatch(clearCart())
+
+    }
   return (
     <div>
       Cart
       <div>
+      <Button onClick={handleClearCart}>Clear Cart</Button>
+      {cartItems.length === 0 && <h2>cart is empty , add something</h2>}
       <ItemList items={cartItems}/>
       </div>
     </div>
